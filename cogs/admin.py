0ba4893fcad2ff756e7ace313708190f40f145e2
ccev <ccev>
2020-03-13 20:56:35 +0100

    fix update error

--- cogs/admin.py
+++ cogs/admin.py
@@ -266,38 +266,38 @@ class Admin(commands.Cog):
         
         print("Looking for updates")
         count = 0
-        for board in boards['raids']:
+        for board in self.bot.boards['raids']:
             if not "title" in board:            
-                boards['raids'][count]['title'] = locale['raids']
+                self.bot.boards['raids'][count]['title'] = locale['raids']
                 print("Updated title for Raid Board")
             if not "ex" in board:
-                boards['raids'][count]['ex'] = False
+                self.bot.boards['raids'][count]['ex'] = False
                 print("Updated ex for Raid Board")
             count += 1
             
-        for board in boards['raid_channels']:
+        for board in self.bot.boards['raid_channels']:
             continue
 
         count = 0    
-        for board in boards['eggs']:
+        for board in self.bot.boards['eggs']:
             if not "title" in board:            
-                boards['eggs'][count]['title'] = locale['eggs']
+                self.bot.boards['eggs'][count]['title'] = locale['eggs']
                 print("Updated title for Egg Board")
             if not "ex" in board:
-                boards['eggs'][count]['ex'] = False
+                self.bot.boards['eggs'][count]['ex'] = False
                 print("Updated ex for Egg Board")
             count += 1
 
         count = 0     
-        for board in boards['stats']:
+        for board in self.bot.boards['stats']:
             if not "title" in board:            
-                boards['stats'][count]['title'] = locale['stats']
+                self.bot.boards['stats'][count]['title'] = locale['stats']
                 print("Updated title for Stat Board")
             count += 1
 
 
         with open("config/boards.json", "w") as f:
-            f.write(json.dumps(boards, indent=4))
+            f.write(json.dumps(self.bot.boards, indent=4))
         await ctx.send("Done.")
 
 def setup(bot):
