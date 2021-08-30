3d967a9e05399094cea362e00dcfe8c065f62228
Alexis Martinez <63435223+alexmartz710@users.noreply.github.com>
2021-08-30 12:36:21 -0400

    Updating max moves

--- discordopole.py
+++ discordopole.py
@@ -22,7 +22,7 @@ extensions = ["cogs.admin", "cogs.boards", "cogs.channels"]
 
 config = util.config.create_config("config/config.ini")
 bot = commands.Bot(command_prefix=config['prefix'], case_insensitive=1)
-bot.max_moves_in_list = 340
+bot.max_moves_in_list = 348
 bot.config = config
 short = pyshorteners.Shortener().tinyurl.short
 
